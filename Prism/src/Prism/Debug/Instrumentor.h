26fef229023d67629e53e448bbbf6c7202d4fb28
linster <lyzw070516@gmail.com>
2026-04-01 18:57:09 +0800

    修复了一些动态链接的问题

--- Prism/src/Prism/Debug/Instrumentor.h
+++ Prism/src/Prism/Debug/Instrumentor.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#pragma once
+#include "Prism/Core/Core.h"
 
 #include <string>
 #include <chrono>
@@ -8,7 +8,7 @@
 
 #include <thread>
 
-namespace Hazel {
+namespace Prism {
 	struct ProfileResult
 	{
 		std::string Name;
@@ -21,101 +21,36 @@ namespace Hazel {
 		std::string Name;
 	};
 
-	class Instrumentor
+	class PRISM_API Instrumentor
 	{
 	private:
 		InstrumentationSession* m_CurrentSession;
 		std::ofstream m_OutputStream;
 		int m_ProfileCount;
 	public:
-		Instrumentor()
-			: m_CurrentSession(nullptr), m_ProfileCount(0)
-		{
-		}
+		Instrumentor();
 
-		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
-		{
-			m_OutputStream.open(filepath);
-			WriteHeader();
-			m_CurrentSession = new InstrumentationSession{ name };
-		}
+		void BeginSession(const std::string& name, const std::string& filepath = "results.json");
 
-		void EndSession()
-		{
-			WriteFooter();
-			m_OutputStream.close();
-			delete m_CurrentSession;
-			m_CurrentSession = nullptr;
-			m_ProfileCount = 0;
-		}
+		void EndSession();
 
-		void WriteProfile(const ProfileResult& result)
-		{
-			if (m_ProfileCount++ > 0)
-				m_OutputStream << ",";
+		void WriteProfile(const ProfileResult& result);
 
-			std::string name = result.Name;
-			std::replace(name.begin(), name.end(), '"', '\'');
+		void WriteHeader();
 
-			m_OutputStream << "{";
-			m_OutputStream << "\"cat\":\"function\",";
-			m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-			m_OutputStream << "\"name\":\"" << name << "\",";
-			m_OutputStream << "\"ph\":\"X\",";
-			m_OutputStream << "\"pid\":0,";
-			m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-			m_OutputStream << "\"ts\":" << result.Start;
-			m_OutputStream << "}";
+		void WriteFooter();
 
-			m_OutputStream.flush();
-		}
-
-		void WriteHeader()
-		{
-			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
-			m_OutputStream.flush();
-		}
-
-		void WriteFooter()
-		{
-			m_OutputStream << "]}";
-			m_OutputStream.flush();
-		}
-
-		static Instrumentor& Get()
-		{
-			static Instrumentor instance;
-			return instance;
-		}
+		static Instrumentor& Get();
 	};
 
-	class InstrumentationTimer
+	class PRISM_API InstrumentationTimer
 	{
 	public:
-		InstrumentationTimer(const char* name)
-			: m_Name(name), m_Stopped(false)
-		{
-			m_StartTimepoint = std::chrono::high_resolution_clock::now();
-		}
+		InstrumentationTimer(const char* name);
 
-		~InstrumentationTimer()
-		{
-			if (!m_Stopped)
-				Stop();
-		}
+		~InstrumentationTimer();
 
-		void Stop()
-		{
-			auto endTimepoint = std::chrono::high_resolution_clock::now();
-
-			long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-
-			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-			Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
-
-			m_Stopped = true;
-		}
+		void Stop();
 	private:
 		const char* m_Name;
 		std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
@@ -125,9 +60,9 @@ namespace Hazel {
 
 #define PR_PROFILE 1
 #if PR_PROFILE
-#define PR_PROFILE_BEGIN_SESSION(name, filepath) ::Hazel::Instrumentor::Get().BeginSession(name, filepath)
-#define PR_PROFILE_END_SESSION() ::Hazel::Instrumentor::Get().EndSession()
-#define PR_PROFILE_SCOPE(name) ::Hazel::InstrumentationTimer timer##__LINE__(name);
+#define PR_PROFILE_BEGIN_SESSION(name, filepath) ::Prism::Instrumentor::Get().BeginSession(name, filepath)
+#define PR_PROFILE_END_SESSION() ::Prism::Instrumentor::Get().EndSession()
+#define PR_PROFILE_SCOPE(name) ::Prism::InstrumentationTimer timer##__LINE__(name);
 #define PR_PROFILE_FUNCTION() PR_PROFILE_SCOPE(__FUNCSIG__)
 #else
 #define PR_PROFILE_BEGIN_SESSION(name, filepath)
